c61a2cf195746b642a4fdd1fb27810449630fee3
vtSSJ <sizykh@gmail.com>
2015-03-02 00:37:30 +0800

    new Project

---
+++ TestWebDAV/MyPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using Sphorium.WebDAV.Server.Framework;
+
+namespace Sphorium.WebDAV.Examples.FileServer
+{
+	public class MyPage
+	{
+		public bool Execute()
+		{
+			var httpApplication = System.Web.HttpContext.Current;
+			var request = System.Web.HttpContext.Current.Request;
+			var isWebDavRequest = false;
+			var requestHttpMethod = request.HttpMethod.ToUpper();
+			switch (requestHttpMethod)
+			{
+				case "GET":
+					//Don't process WebDAV requests for the root and for files that exist (ie. default.aspx)
+					var mappedPath = httpApplication.Server.MapPath(request.Url.LocalPath);
+					isWebDavRequest = !File.Exists(mappedPath);
+					break;
+				case "PUT":
+				case "HEAD":
+				case "OPTIONS":
+				case "LOCK":
+				case "UNLOCK":
+				case "PROPFIND":
+				case "PROPPATCH":
+					isWebDavRequest = true;
+					break;
+			}
+			if (!isWebDavRequest)
+				return false;
+			var webDavProcessor = new WebDavProcessor();
+			webDavProcessor.ProcessRequest(httpApplication.ApplicationInstance);
+			return true;
+		}
+	}
+}
---
+++ TestWebDAV/default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" %>
+<%@ Import Namespace="Sphorium.WebDAV.Examples.FileServer" %>
+<%
+    var o = new MyPage();
+//    if (!o.Execute())
+    { %>
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+        <head runat="server">
+	        <title>WebDAV.NET Framework Example</title>
+        </head>
+        <body>
+		        <h4 align="center">
+			        This is a simple example on how to use the WebDAV Server framework<br />
+		        </h4>
+		        <p align="center">
+		            Open file: <a href="ms-word:http://localhost:49416/tz7.doc">Test</a>
+		        </p>
+        </body>
+        </html>
+    <% }
+%>
